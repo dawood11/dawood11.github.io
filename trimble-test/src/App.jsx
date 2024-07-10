00df3a567bb2fcebcf12e80eb4b8f20535b8326d
Dawood Ahmad <extdah@brreg.no>
2024-07-10 18:18:10 +0200

    dotConnect check

--- trimble-test/src/App.jsx
+++ trimble-test/src/App.jsx
@@ -24,36 +24,39 @@ function App() {
     );
   }
 
-  const setMenu = () => {
-    alert("MENYYY");
-    dotConnect().then((WorkspaceAPI) => {
-      const mainMenuObject = {
-        title: "DET - TC-Extensions",
-        icon: "https://de-tech.no/trimble-connect-web-extensions/favicon.png",
-        command: "main_nav_menu_cliked",
-        subMenus: [
-          {
-            title: "IFC-Tool",
-            icon: "https://de-tech.no/trimble-connect-web-extensions/submenu_1.png",
-            command: "submenu_1_clicked",
-          },
-          {
-            title: "DWG-Tool",
-            icon: "https://de-tech.no/trimble-connect-web-extensions/submenu_2.png",
-            command: "submenu_2_clicked",
-          },
-        ],
-      };
-      WorkspaceAPI.ui.setMenu(mainMenuObject);
-    });
-  }
+  // const setMenu = () => {
+  //   alert("MENYYY");
+  //   dotConnect().then((WorkspaceAPI) => {
+  //     const mainMenuObject = {
+  //       title: "DET - TC-Extensions",
+  //       icon: "https://de-tech.no/trimble-connect-web-extensions/favicon.png",
+  //       command: "main_nav_menu_cliked",
+  //       subMenus: [
+  //         {
+  //           title: "IFC-Tool",
+  //           icon: "https://de-tech.no/trimble-connect-web-extensions/submenu_1.png",
+  //           command: "submenu_1_clicked",
+  //         },
+  //         {
+  //           title: "DWG-Tool",
+  //           icon: "https://de-tech.no/trimble-connect-web-extensions/submenu_2.png",
+  //           command: "submenu_2_clicked",
+  //         },
+  //       ],
+  //     };
+  //     WorkspaceAPI.ui.setMenu(mainMenuObject);
+  //   });
+  // }
+
   function getCurrentProjectFromTrimple() {
     console.log("GET PROJECT INFOOOO");
     alert("GET PROJECT INFOOOO");
-    dotConnect().then((WorkspaceAPI) => {
-      console.log(WorkspaceAPI.project.getCurrentProject());
-      alert(WorkspaceAPI.project.getCurrentProject());
-    });
+    const data = dotConnect();
+    console.log(data)
+    // .then((WorkspaceAPI) => {
+    //   console.log(WorkspaceAPI.project.getCurrentProject());
+    //   alert(WorkspaceAPI.project.getCurrentProject());
+    // });
   }
 
   return (

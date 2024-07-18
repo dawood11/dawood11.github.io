358b46b5a2c2abf69bb256c6f35bc43edaa83451
bimyr <yasin.rafiq@outlook.com>
2024-07-18 21:20:45 +0200

    beta

--- trimble-test/src/App.jsx
+++ trimble-test/src/App.jsx
@@ -420,7 +420,7 @@ function App() {
         <footer>
           <img src="https://dawood11.github.io/trimble-test/src/assets/Logo_Haehre.png" alt="Logo" className="footer-logo"/>
           <p>Utviklet av Yasin Rafiq</p>
-          <p>Beta 1.6</p>
+          <p>Beta 1.0</p>
         </footer>
       </div>
     </>

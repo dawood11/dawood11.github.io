9d5159200797598b04809e59db0726b5e30be21e
bimyr <yasin.rafiq@outlook.com>
2024-07-23 21:34:39 +0200

    admn 1

--- trimble-test/src/main.jsx
+++ trimble-test/src/main.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
-import App from './App.jsx'
-import AdminPanel from './AdminPanel.jsx'
-import Login from './Login.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import App from './App.jsx';
+import AdminPanel from './AdminPanel.jsx';
+import Login from './Login.jsx';
+import './index.css';
 
-const isAuthenticated = () => {
-  return localStorage.getItem('auth') === 'true';
-}
+const isAuthenticated = () => localStorage.getItem('auth') === 'true';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -16,8 +14,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={isAuthenticated() ? <AdminPanel /> : <Navigate to="/login" />} />
+        <Route
+          path="/admin"
+          element={isAuthenticated() ? <AdminPanel /> : <Navigate to="/login" />}
+        />
       </Routes>
     </Router>
   </React.StrictMode>
-)
+);

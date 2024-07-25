df3dab424a57b589a263cae13629c6d032bec2a0
bimyr <yasin.rafiq@outlook.com>
2024-07-25 23:30:36 +0200

    gm6

--- trimble-test/src/App.jsx
+++ trimble-test/src/App.jsx
@@ -237,7 +237,7 @@ class App extends Component {
       console.log(`Applying ghost mode to non-selected objects: ${JSON.stringify(nonSelectedEntities)}`);
 
       // Apply ghost mode to the non-selected objects
-      await api.presentation.ghost({ entities: nonSelectedEntities, ghost: true });
+      await api.viewer.presentation.ghost({ ghost: true });
       console.log(`Applied ghost mode to non-selected objects.`);
     } catch (error) {
       console.error("Error applying ghost mode:", error);

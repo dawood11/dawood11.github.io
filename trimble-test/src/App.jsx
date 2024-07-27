eb0eb56582964946cf0050fdd935705980388e22
bimyr <yasin.rafiq@outlook.com>
2024-07-27 22:21:45 +0200

    v1

--- trimble-test/src/App.jsx
+++ trimble-test/src/App.jsx
@@ -57,24 +57,17 @@ class App extends Component {
   };
 
   getAttributeDataFromTrimble = async () => {
+    const posAttributes = ["Pos.nr.", "Pos.nr", "Pos nr.", "Pos"];
     const dimensionAttributes = ["Diameter", "DIM A", "DIM B", "DIM C", "DIM R"];
-    const attributesToCheck = [
-      "K21 Pos.nr.",
-      "Pos.nr.",
-      "Pos",
-      "Posnummer",
-      "Pos.nr",
-      "Posnr.",
-      "Pos nr.",
-      "K-M01 Pos.nr."
-    ];
 
+    console.log("GET ATTRIBUTE DATA");
     const api = await this.dotConnect();
     await this.getProjectId();
     await this.getModelName();
     await this.getViews();
 
     const viewerObjects = await api.viewer.getObjects();
+    console.log("viewerObjects: ", viewerObjects);
 
     const attributeObjects = [];
     const batchSize = 1000;
@@ -82,10 +75,12 @@ class App extends Component {
     for (const modelObjectsSet of viewerObjects) {
       const modelId = modelObjectsSet["modelId"];
       let modelObjectIdsList = modelObjectsSet["objects"].map((obj) => obj.id);
+      console.log("Fetching properties for model ID:", modelId);
 
       for (let i = 0; i < modelObjectIdsList.length; i += batchSize) {
         const batch = modelObjectIdsList.slice(i, i + batchSize);
         const properties = await api.viewer.getObjectProperties(modelId, batch);
+        console.log("Fetched properties:", properties);
 
         properties.forEach((propertySet) => {
           if (propertySet.properties) {
@@ -99,22 +94,22 @@ class App extends Component {
             };
 
             propertySet.properties.forEach((prop) => {
-              attributesToCheck.forEach((attribute) => {
-                if (prop.name === attribute) {
+              prop.properties.forEach((subProp) => {
+                if (posAttributes.some(attr => subProp.name.includes(attr))) {
                   primaryAttribute = { 
                     modelId, 
                     id: propertySet.id, 
                     class: propertySet.class, 
-                    name: prop.name,
-                    value: prop.value 
+                    name: subProp.name,
+                    value: subProp.value 
                   };
                 }
-              });
 
-              dimensionAttributes.forEach(dimAttr => {
-                if (prop.name.includes(dimAttr)) {
-                  dimensions[dimAttr] = prop.value;
-                }
+                dimensionAttributes.forEach(dimAttr => {
+                  if (subProp.name.includes(dimAttr)) {
+                    dimensions[dimAttr] = subProp.value;
+                  }
+                });
               });
             });
 
@@ -127,6 +122,7 @@ class App extends Component {
     }
 
     this.setState({ attributeData: attributeObjects });
+    console.log("Attribute Objects: ", attributeObjects);
   };
 
   handleGroupClick = async (value) => {
@@ -159,11 +155,15 @@ class App extends Component {
       return acc;
     }, []);
 
+    console.log(`Selecting objects: ${JSON.stringify(modelEntities)}`);
+
     // Show only the selected objects
     await api.viewer.isolateEntities(modelEntities);
-
+    console.log(`Isolated selected objects.`);
+  
     // Fit the view to the selected objects
     await api.viewer.setCamera("reset");
+    console.log(`View reset to fit selected objects.`);
   };
 
   deselectObjects = async (api, objects) => {
@@ -176,6 +176,7 @@ class App extends Component {
       modelObjectIds: modelEntities
     };
     await api.viewer.setSelection(objectSelector, "remove");
+    console.log(`Objects deselected.`);
   };
 
   createView = async () => {
@@ -183,6 +184,7 @@ class App extends Component {
     const selectedData = this.state.attributeData.filter(obj => this.state.selectedGroups[obj.value]);
 
     if (selectedData.length === 0) {
+      console.log("No objects selected to create a view.");
       return;
     }
 
@@ -198,8 +200,10 @@ class App extends Component {
     };
 
     const viewSpec = await api.view.createView(viewInfo);
+    console.log(`View created with objects:`, viewSpec.objects);
 
     await api.view.setView(viewSpec.id);
+    console.log(`View set as active.`);
   };
 
   groupAttributeData = (data = this.state.attributeData) => {
@@ -337,6 +341,7 @@ class App extends Component {
 
     const filename = `${this.state.modelName}_Bøyeliste.xlsx`;
     saveAs(blob, filename);
+    console.log("Exported data to Excel");
   };
 
   renderGroupedAttributeObjects = () => {

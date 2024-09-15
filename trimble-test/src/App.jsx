da3ce81de34bf0627bb1a5de6ec797b1c4dc32a4
bimyr <yasin.rafiq@outlook.com>
2024-09-15 11:33:26 +0200

    BETA 1.8

--- trimble-test/src/App.jsx
+++ trimble-test/src/App.jsx
@@ -318,7 +318,7 @@ class App extends Component {
           <footer>
             <img src="https://dawood11.github.io/trimble-test/src/assets/Logo_Haehre.png" alt="Logo" className="footer-logo"/>
             <p>Utviklet av Yasin Rafiq</p>
-            <p>BETA 1.8</p>
+            <p>Beta 1.8</p>
           </footer>
         </div>
       </>

7a9839409421f4f5328c1c3f6940cd14c3aac49b
bimyr <yasin.rafiq@outlook.com>
2024-10-12 16:22:06 +0200

    0.4.1

--- trimble-test/src/App.jsx
+++ trimble-test/src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
       </header>
       <main className="content">
         {/* Content Tree Component */}
-        <modus-tree-view multi-selection="true" checkbox-selection="true">
+        <modus-tree-view borderless="true">
           <modus-tree-view-item node-id="1" label="Object ID: 1">
             <modus-tree-view-item node-id="2" label="Pos.nr: 1234" />
             <modus-tree-view-item node-id="3" label="Pos.nr: 5678" />
@@ -45,7 +45,7 @@ const App = () => {
       <footer>
         <img src="https://dawood11.github.io/trimble-test/src/assets/Logo_Haehre.png" alt="Logo" className="footer-logo" />
         <p>Utviklet av Yasin Rafiq</p>
-        <p>UTVIKLING 0.4.0</p>
+        <p>UTVIKLING 0.4.1</p>
       </footer>
     </div>
   );
